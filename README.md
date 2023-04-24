72d917ce2c5720e373356c6312d3d2a55badfd2b
BARALLL <54141221+BARALLL@users.noreply.github.com>
2023-04-24 12:20:21 +0200

    up

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
 🛠️ File analysis in progress 🛠️
 
-Blender Addon to manipulate shp0 files, used for polygon morphing and vertex displacement animations.
+Blender Addon to create and manipulate shp0 files, used for polygon morphing and vertex displacement animations.
 Reverse engineering process to understand the .shp0 file format from hex.
+
+
+    roadmap:
+        + is done
+        * partially done
+        - needs to be done
+
+        Priorities:
+            verify is animation data/entry correctly written to file
+            section 1
+            update offsets
+
+        
+        section 0:
+            file header (just need length of file update with                                           +
+            ftell() then fseek(0x04) once all the file is written)
+            
+            shp0 header                                                                                 +
+        
+            Animation Data
+                use of shape keys for animation entries
+                0x00 bitflags                                                                           +
+                (but still need the update animation button, hopefully find a way to get rid of it)
+                and better coord between vg size and animIndex (off by 1 when size vg -1)               *
+                
+                0x04/8? name offset to section one vg name                                              *
+                0x0A number of shape keys   (len(bpy.types.ShapeKey))                                   *
+                
+                0x0C-0x10-0x14 TBD                                                                      -
+            
+            Animation Entry
+                "header"
+                    this shape key number of kf                                                         +
+                    padding                                                                             -
+                    1/(max kf number)                                                                   +
+                followed by kf entries: (size N*12)
+                    frame number                                                                        +
+                    shape key animation value (0-1 morph)                                               +
+                    interpolation tangent value (f-curve or shapeKey.interpolation data struct?)        *
+                    
+        section 1:
+            list of vertex group names                                                                  -
--- shp0 editor.py
+++ shp0 editor.py
@@ -21,8 +21,8 @@ class Exportshp0(bpy.types.Operator, ExportHelper):
     def write_file_header(context, f, setting):
         f.write(struct.pack(">4s", b"SHP0")) # magic number
         f.write(struct.pack(">i", 0)) # placeholder for file length
-        f.write(struct.pack(">i", setting.version)) # SHP0 version number
-        f.write(struct.pack(">i", 0)) # offset to BRRES file
+        f.write(struct.pack(">i", setting.version)) # version number
+        f.write(struct.pack(">i", 0)) # offset to brres file
         f.write(struct.pack(">4i", 0,0,0,0)) # placeholder for section offsets
         f.write(struct.pack(">i", 0)) #placeholder for offset to file name location in file
         
@@ -36,27 +36,100 @@ class Exportshp0(bpy.types.Operator, ExportHelper):
         #need to check if works bc loop is bool => correctly translated to 0x00/0x01 ?
 
 
-    def update_file_length(context, f, setting):
+    def update_file_length(context, f, setting, offset_placement):
         length = f.tell()
-        f.seek(4, 0)
+        f.seek(offset_placement, 0)
         f.write(struct.pack(">i", length))
+        offset_placement += 4
+        f.seek(length, 0)
         
+    
+    
+    def write_anim_data(context, f, setting, index):
         
+        if context.object.vertex_groups.active_index != -1:
+            #strange way to get the value: enable='OPT_A' (str) so enable[4]= 'A'
+            #ord() to get the ascii value then -64 to get 1 => 0x02
+            enable_value = 2**(ord(context.scene.animGroup[index].enable[4]) - 64)
+            f.write(struct.pack(">i", enable_value)) #correct 0x0_ value ?
+        else:
+            f.write(struct.pack(">i", 0))
+            print("No vertex group found, exported as 0")
+        index += 1
+        
+        f.write(struct.pack(">i", 0)) #placeholder for vg name (name of the target vertex set) offset
+        f.write(struct.pack(">H", index))
+        f.write(struct.pack(">H", len(context.object.data.shape_keys.key_blocks)))
+        
+        f.write(struct.pack(">i", 0)) #placeholder for 0x0C
+        f.write(struct.pack(">i", 0)) #placeholder for 0x10
+        f.write(struct.pack(">i", 0))
+        for i in range(len(context.object.data.shape_keys.key_blocks)):
+            f.write(struct.pack(">i", 0)) #placeholder for 0x14 offsets
+    
+    
+    
+      
+      
+    def write_anim_entry(self, context, f, setting):
+        obj = context.active_object
+        #'if sk is not None:' not needed bc already checked in write_shp0_file        
+        sk = obj.data.shape_keys
+        anim = sk.animation_data
+        if anim is not None:
+            anim = anim.action
+            if anim is not None:
+                for fcu in anim.fcurves:
+                    f.write(struct.pack(">H", 0)) #nb kf
+                    f.write(struct.pack(">H", 0)) #padding?
+                    f.write(struct.pack(">f", 0)) #float
+                    for kf in fcu.keyframe_points:
+                        f.write(struct.pack(">f", sk.key_blocks.frame)) #kf.index, surely not expected value
+                        f.write(struct.pack(">f", sk.key_blocks.value)) #kf.value
+                        f.write(struct.pack(">f", evaluate(kf)))
+        else:
+            self.report({'ERROR'}, "No animation found on active object")
+            return {'CANCELLED'}
+
+        return {'FINISHED'}
+    
+        #write_kf(context, f, setting)
+    
+
+    
+    def write_vertex_data_names(context, f, setting):    #section 1
+        Exportshp0.update_file_length(context, f, setting, 4)
+        
+    
+    
+    
     def write_shp0_file(self, context, setting):
         print("running write_shp0_file...")
         with open(self.filepath, 'wb') as f:
-        
+            nb = 4
             Exportshp0.write_file_header(context, f, setting)
             Exportshp0.write_shp0_header(context, f, setting)
-            Exportshp0.update_file_length(context, f, setting)
-        
+            
+            #Exportshp0.update_file_length(context, f, setting, nb*4+16)
+            #Exportshp0.update_file_length(context, f, setting, nb*4+16)
+            
+            if context.object.data.shape_keys is not None:
+                index = 0
+                for sk in context.object.data.shape_keys.key_blocks:
+                    Exportshp0.write_anim_data(context, f, setting, index)
+                    Exportshp0.write_anim_entry(self, context, f, setting)
+            else:
+                self.report({'ERROR'}, "No shape key found on active object")
+                return {'CANCELLED'}
+            
             #for animations in animList
                 #write anim header
                 #write anim entry header
                 #for kf in kfList
                     #write frame number, anim value, hermite slope interpolation
-
-
+        
+            Exportshp0.update_file_length(context, f, setting, 4)
+        self.report({'INFO'}, ".shp0 exported successfully!")
         return {'FINISHED'}    
         
         
@@ -219,37 +292,52 @@ if __name__ == "__main__":
 
 '''
 roadmap:
-    currently done:
-        file header (but length of file still need to be done with
+    + is done
+    * partially done
+    - needs to be done
+
+    Priorities:
+        verify is animation data/entry correctly written to file
+        section 1
+        update offsets
+
+    
+    section 0:
+        file header (just need length of file update with                                           +
         ftell() then fseek(0x04) once all the file is written)
         
-        shp0 header
-
-    need to be done:
-        section 0:
-            Animation Data
-                use of shape keys for animation entries
-                0x00 bitflags: done 
-                (but still need the update animation button, 
-                hopefully find a way to get rid of it)
-                and better coord between vg size and animIndex (off by 1 when size vg -1)
-                
-                0x04/8? name offset to section one vg name
-                0x0A number of shape keys
-                
-                0x0C-0x10-0x14 TBD
+        shp0 header                                                                                 +
+    
+        Animation Data
+            use of shape keys for animation entries
+            0x00 bitflags                                                                           +
+            (but still need the update animation button, hopefully find a way to get rid of it)
+            and better coord between vg size and animIndex (off by 1 when size vg -1)               *
             
-            Animation Entry
-                "header"
-                    this shape key number of kf
-                    padding
-                    1/(max kf number)
-                followed by kf entries: (size N*12)
-                    frame number
-                    shape key animation value (0-1 morph)
-                    interpolation tangent value (f-curve or shapeKey.interpolation data struct?)
-                    
-        section 1:
-            list of vertex group names 
+            0x04/8? name offset to section one vg name                                              *
+            0x0A number of shape keys   (len(bpy.types.ShapeKey))                                   *
             
+            0x0C-0x10-0x14 TBD                                                                      -
+        
+        Animation Entry
+            "header"
+                this shape key number of kf                                                         +
+                padding                                                                             -
+                1/(max kf number)                                                                   +
+            followed by kf entries: (size N*12)
+                frame number                                                                        +
+                shape key animation value (0-1 morph)                                               +
+                interpolation tangent value (f-curve or shapeKey.interpolation data struct?)        *
+                
+    section 1:
+        list of vertex group names                                                                  -
+            
+        
+    
+    change animGroup to shape keys instead of vg ?
+            
+    Animation Data <=> shape keys
+    Animations Entry <=> fcurve of shape key (animation data)
+    keyframe <=> keyframe of fcurve (animation entry)
+  
 '''
